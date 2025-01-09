94f7861e77548d8f7fb9b1571e6e060233cb27ba
Alvin Rachman Septian <alvinrachman16@gmail.com>
2025-01-09 13:11:33 +0700

    update data about

--- src/data/DataAbout.jsx
+++ src/data/DataAbout.jsx
@@ -7,8 +7,11 @@ import { SiJquery } from "react-icons/si";
 import { FaPhp } from "react-icons/fa";
 import { SiJavascript } from "react-icons/si";
 import { FaGithub } from "react-icons/fa";
+import { BiLogoPostgresql } from "react-icons/bi";
 import { GrMysql } from "react-icons/gr";
 import { VscVscode } from "react-icons/vsc";
+import { SiPostman } from "react-icons/si";
+import { SiDbeaver } from "react-icons/si";
 
 export const dataAbout = [
   `My name is Alvin Rachman Septian, I'm a Web Developer and graduated
@@ -25,59 +28,74 @@ export const dataAboutTech = [
   {
     label: "Golang",
     icon: <FaGolang />,
-    bgColor: "#00ADD8"
+    bgColor: "#00ADD8",
   },
   {
     label: "ReactJS",
     icon: <GrReactjs />,
-    bgColor: "#61DAFB"
+    bgColor: "#61DAFB",
   },
   {
     label: "Tailwind",
     icon: <SiTailwindcss />,
-    bgColor: "#38BDF8"
+    bgColor: "#38BDF8",
   },
   {
     label: "Bootstrap",
     icon: <FaBootstrap />,
-    bgColor: "#7952B3"
+    bgColor: "#7952B3",
   },
   {
     label: "Codeigniter",
     icon: <DiCodeigniter />,
-    bgColor: "#E24C2C"
+    bgColor: "#E24C2C",
   },
   {
     label: "Jquery",
     icon: <SiJquery />,
-    bgColor: "#0769AD"
+    bgColor: "#0769AD",
   },
   {
     label: "PHP",
     icon: <FaPhp />,
-    bgColor: "#8993BE"
+    bgColor: "#8993BE",
   },
   {
     label: "Javascript",
     icon: <SiJavascript />,
-    bgColor: "#F7DF1E"
+    bgColor: "#F7DF1E",
   },
 ];
 
 export const dataAboutTools = [
-    {
-        label: "GitHub",
-        icon: <FaGithub />,
-        bgColor: "#000"
-    },
-    {
-        label: "MySQL",
-        icon: <GrMysql />,
-        bgColor: "#00618A"
-    },
-    {
-        label: "VSCode",
-        icon: <VscVscode />,
-        bgColor: "#007ACC"
-    },
-]
+  {
+    label: "GitHub",
+    icon: <FaGithub />,
+    bgColor: "#000",
+  },
+  {
+    label: "PostgreSQL",
+    icon: <BiLogoPostgresql />,
+    bgColor: "#336791",
+  },
+  {
+    label: "MySQL",
+    icon: <GrMysql />,
+    bgColor: "#00618A",
+  },
+  {
+    label: "VSCode",
+    icon: <VscVscode />,
+    bgColor: "#007ACC",
+  },
+  {
+    label: "Postman",
+    icon: <SiPostman />,
+    bgColor: "#FF6C37",
+  },
+  {
+    label: "DBeaver",
+    icon: <SiDbeaver />,
+    bgColor: "#1D5F8A",
+  },
+];
